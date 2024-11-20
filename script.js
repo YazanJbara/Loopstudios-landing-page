96885b9b6e62be38c925485f6cb226fa9cc47bd1
Yazan <yazanjbara20034456@gmail.com>
2024-11-20 08:16:54 +0300

    small tweaks

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
-'use strict';
+
 const menuIcon = document.querySelector('.menu-icon');
 const closeIcon = document.querySelector('.close-icon');
 const overlay = document.querySelector('.overlay');
 const navItems = document.querySelector('.navigation-items');
 
-menuIcon.addEventListener('click', () => { 
-    menuIcon.style.display = 'none';
-    closeIcon.style.display = 'block';
-    overlay.style.display = 'block';
-    navItems.style.display = 'block';
+menuIcon.addEventListener('click', () => {
+  menuIcon.style.display = 'none';
+  closeIcon.style.display = 'block';
+  overlay.style.display = 'block';
+  navItems.style.display = 'block';
 });
 
 closeIcon.addEventListener('click', () => {
-    closeIcon.style.display = 'none';
-    menuIcon.style.display = 'block';
-    overlay.style.display = 'none';
-    navItems.style.display = 'none';
+  closeIcon.style.display = 'none';
+  menuIcon.style.display = 'block';
+  overlay.style.display = 'none';
+  navItems.style.display = 'none';
+});
+
+// adding tab index to every img on the page to make it more accessible
+const images = document.querySelectorAll('img');
+images.forEach((img, index) => {
+  img.setAttribute('tabindex', 0);
 });
